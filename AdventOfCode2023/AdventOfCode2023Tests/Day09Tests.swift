3c4a9b17ecc03aa897f6fdbc3400871d5fc75017
Dan Hart <dan-hart@users.noreply.github.com>
2023-12-10 13:23:09 -0600

    Day 09

---
+++ AdventOfCode2023/AdventOfCode2023Tests/Day09Tests.swift
@@ -0,0 +1,13 @@
+//
+//  Day09Tests.swift
+//  AdventOfCode2023Tests
+//
+//  Created by Dan Hart on 12/9/23.
+//
+
+import XCTest
+@testable import aoc2023core
+
+final class Day09Tests: XCTestCase {
+
+}
---
+++ aoc2023core/Sources/aoc2023core/Day09.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dan Hart on 12/9/23.
+//
+
+import Foundation
+
+public class Day09: Challenge {
+    public var name: String = "Day 9"
+    public init() {}
+
+    public func run(with input: [String], also rawInput: String) {
+    }
+}
